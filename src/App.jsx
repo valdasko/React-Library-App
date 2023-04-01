129e0a655f7be149716eb8a7a90adbf4745478c1
Valdas <valdaskontenis1@gmail.com>
2023-04-01 18:33:03 +0300

    pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './styles/reset.css';
 import './styles/App.css';
+import Header from './components/layout/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
+import AddBook from './components/pages/AddBook';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>New Project</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/addnewbook" element={AddBook} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
